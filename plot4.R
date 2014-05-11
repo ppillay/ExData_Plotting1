160b4d3fb57e4844b85c465047c798abc820eb2f
Prabha Pillay <ppillay@dius.com.au>
2014-05-12 05:54:49 +1000

    Removed border from legend box in plot 4

--- plot4.R
+++ plot4.R
@@ -14,7 +14,7 @@ if(file.exists("household_power_consumption.txt")) {
     plot(dateTime, Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
     lines(dateTime,Sub_metering_2,col="red")
     lines(dateTime,Sub_metering_3,col="blue")
-    legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+    legend("topright",box.lwd=0,lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
     plot(dateTime, Voltage,type="l",xlab="datetime",ylab="Voltage")  
     plot(dateTime, Global_reactive_power,axes=TRUE,ann=TRUE,type="l",xlab="datetime")
   })
